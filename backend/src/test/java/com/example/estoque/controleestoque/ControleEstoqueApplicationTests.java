f75b6aa05e63492dae676ec15f79584433616ea1
willian rafael <willian.rafael18@gmail.com>
2022-09-11 12:09:41 -0300

    Project Created

---
+++ backend/src/test/java/com/example/estoque/controleestoque/ControleEstoqueApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.estoque.controleestoque;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ControleEstoqueApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
